2e346877dc57529be13062fd52485ac94395bd93
Luke Horwell <code@horwell.me>
2018-05-25 12:01:35 +0100

    Add script for local development.

---
+++ local-preview.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# If Jekyll is installed on your computer, you can run this script to preview
+# the website locally.
+#
+# If this fails, try "bundle install" first.
+#
+
+bundle exec jekyll serve --watch --livereload
+
